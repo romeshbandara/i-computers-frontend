80802634b48c74a5e0c7e486b5585208f2677467
romeshbandara <official.romeshbandara@gmail.com>
2026-09-17 23:14:53 +0530

    landing component created ✏

---
+++ pages/landingPage.jsx
@@ -0,0 +1,7 @@
+export default function LandingPage(){
+    return(
+        <div className="w-full h-screen bg-primary flex items-center justify-center">
+            <h1 className="text-4xl text-white">Landing Page</h1>
+        </div>
+    )
+}
